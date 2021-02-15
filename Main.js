7f2215fbeda1f973331617d04042ff62ec448859
Joseph <josephtsegen10@gmail.com>
2021-02-15 01:24:22 +0100

    take 3 on solving animation issues

--- Main.js
+++ Main.js
@@ -1,7 +1,3 @@
-// B-lazy
-var bLazy = new Blazy({
-	// Options
-});
 // Selectors
 const toggleID = document.querySelector('#nav-toggle');
 const navMenuId = document.querySelector('#nav-menu');
@@ -41,33 +37,35 @@ navMenuId.addEventListener('click', addActive);
 toggleID.addEventListener('click', showMenu);
 
 // Animation effect
-const sr = ScrollReveal({
-	origin: 'top',
-	distance: '80px',
-	duration: 2000,
-	reset: true,
-});
+window.onload = () => {
+	const sr = ScrollReveal({
+		origin: 'top',
+		distance: '80px',
+		duration: 2000,
+		reset: true,
+	});
 
-// scroll reveal animation
-sr.reveal('.home-title', {});
-sr.reveal('.button', { delay: 200 });
-sr.reveal('.home-img', { delay: 400 });
-sr.reveal('.home-social-icon', { interval: 200 });
+	// scroll reveal animation
+	sr.reveal('.home-title', {});
+	sr.reveal('.button', { delay: 200 });
+	sr.reveal('.home-img', { delay: 400 });
+	sr.reveal('.home-social-icon', { interval: 200 });
 
-sr.reveal('.about-img', {});
-sr.reveal('.about-subtitle', { delay: 200 });
-sr.reveal('.about-text', { delay: 400 });
+	sr.reveal('.about-img', {});
+	sr.reveal('.about-subtitle', { delay: 200 });
+	sr.reveal('.about-text', { delay: 400 });
 
-sr.reveal('.skills-subtitle', {});
-sr.reveal('.skills-text', { delay: 200 });
-sr.reveal('.skills-data', { delay: 400 });
-sr.reveal('.skills-img', { delay: 400 });
+	sr.reveal('.skills-subtitle', {});
+	sr.reveal('.skills-text', { delay: 200 });
+	sr.reveal('.skills-data', { delay: 400 });
+	sr.reveal('.skills-img', { delay: 400 });
 
-sr.reveal('.work-img', { interval: 200 });
-sr.reveal('.contact-input', { interval: 200 });
+	sr.reveal('.work-img', { interval: 200 });
+	sr.reveal('.contact-input', { interval: 200 });
 
-sr.reveal('.footer-title', { origin: 'left', duration: 2000, delay: 500 });
-sr.reveal('#facebook-icon', { origin: 'left', duration: 2200, delay: 1000 });
-sr.reveal('#whatsapp-icon', { origin: 'left', duration: 2300, delay: 2000 });
-sr.reveal('#linkedin-icon', { origin: 'left', duration: 2400, distance: '90px', delay: 3000 });
-sr.reveal('#telegram-icon', { origin: 'left', duration: 2500, distance: '100px', delay: 4000 });
+	sr.reveal('.footer-title', { origin: 'left', duration: 2000, delay: 500 });
+	sr.reveal('#facebook-icon', { origin: 'left', duration: 2200, delay: 1000 });
+	sr.reveal('#whatsapp-icon', { origin: 'left', duration: 2300, delay: 2000 });
+	sr.reveal('#linkedin-icon', { origin: 'left', duration: 2400, distance: '90px', delay: 3000 });
+	sr.reveal('#telegram-icon', { origin: 'left', duration: 2500, distance: '100px', delay: 4000 });
+};
