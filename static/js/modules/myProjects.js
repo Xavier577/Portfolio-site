0f2bda467fcec0b9223cabd45e59e8121f4fe039
joseph <josephtsegen10@gmail.com>
2021-11-19 23:24:29 +0100

    fixed pushing margin issue maybe

--- static/js/modules/myProjects.js
+++ static/js/modules/myProjects.js
@@ -11,19 +11,18 @@ const MyProjects = {
     url: "https://xavier577.github.io/Covid-19-tracker/",
     description: `This is a react application i made that made use of the disease.sh api to
     retrieve the information  displayed on the site. The project helped me learn about apis
-    and the importance of making a good frontends with good user exprience to display the 
+    and the importance of making  good frontends with good user exprience to display the 
     information provided by the api.`,
     name: "covid 19 tracker",
   },
   project3: {
     previewImgSrc: "./assets/images/projects_previews/project3.png",
     url: "https://jvert.netlify.app",
-    description: `Another react web app as react is my main frontend framework. 
-    I made this project with the aim of showing how important data is to the frontend. 
-    Using the currencyfreaks api i was able to turn data into useful information in form 
-    of the bar chart showing the top 10 currencies, the table show the rates per usd and
-     the primary feature the converter which converts one currency to another using the 
-     information about the rates provided by the api. This project taught me more about apis and also how to 
+    description: `
+    This is another react project i made using the currencyfreaks api as my source of data.
+    Using the convert you can convert one currency to another, see the top 10 currency 
+    which is displayed to the bar chart and the rates for each currency on the table.
+     This project taught me more about apis and also how to 
      implement light and dark mode for web applications`,
     name: "Jvert",
   },
@@ -39,10 +38,10 @@ const MyProjects = {
   project5: {
     previewImgSrc: "./assets/images/projects_previews/project5.png",
     url: "https://www.npmjs.com/package/exode",
-    description: `Exode cli tool is a cli tool that i made to solve a common problem i ran into whenever using typescript on the backend with nodejs, the overhead with setting up configurations 
-and boilerplates. So i decided to make i tool i could use to kick start all my express node js application with typescript and i still use till this day. This project taught what nodejs
-is really capable of, i learnt a lot of core modules such as execSync, child_process and more about streams. It was really exciting for me to build this out of my curiousity and it gave
-me such pleasure. This package is avaliable on npm and can be used by anyone using the npx command.`,
+    description: `This is a cli tool i made to automate the process of kick starting my express nodejs 
+    applications with typescript to take care of the setting up boilerplates and configurations. 
+    This package is avaliable on npm and can be used by anyone.
+    I learnt more about processes, streams and running commands via nodejs from building this project`,
     name: "Exode cli",
   },
   project6: {
